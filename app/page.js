89f6ae88492794f9c7a7ec609d583b2d58311ca0
gabewin <hello@gabewin.com>
2024-08-16 15:17:43 +0200

    everything works except save

---
+++ app/page.js
@@ -0,0 +1,127 @@
+
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { Container, Box, Typography, AppBar, Toolbar, Button, Grid } from '@mui/material'
+import getStripe from '@/utils/get-stripe'
+
+const handleSubmit = async () => {
+  const checkoutSession = await fetch('/api/checkout_sessions', {
+    method: 'POST',
+    headers: { origin: 'http://localhost:3000' },
+  })
+  const checkoutSessionJson = await checkoutSession.json()
+
+  const stripe = await getStripe()
+  const {error} = await stripe.redirectToCheckout({
+    sessionId: checkoutSessionJson.id,
+  })
+
+  if (error) {
+    console.warn(error.message)
+  }
+}
+
+export default function Home() {
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{flexGrow: 1}}>
+            Flashcard SaaS
+          </Typography>
+          <SignedOut>
+            <Button color="inherit" href="/sign-in">Login</Button>
+            <Button color="inherit" href="/sign-up">Sign Up</Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{textAlign: 'center', my: 4}}>
+        <Typography variant="h2" component="h1" gutterBottom>
+          Welcome to Flashcard SaaS
+        </Typography>
+        <Typography variant="h5" component="h2" gutterBottom>
+          The easiest way to create flashcards from your text.
+        </Typography>
+        <Button variant="contained" color="primary" sx={{mt: 2, mr: 2}} href="/generate">
+          Get Started
+        </Button>
+        <Button variant="outlined" color="primary" sx={{mt: 2}}>
+          Learn More
+        </Button>
+      </Box>
+
+      <Box sx={{my: 6, textAlign: 'center'}}>
+        <Typography variant="h4" component="h2" gutterBottom>Features</Typography>
+        <Grid container spacing={4} justifyContent="center">
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Smart Flashcards</Typography>
+            <Typography>
+              {' '}
+              Our AI intelligently breaks down your text into concise flashcrds, perfect for studying
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Accessible Anywhere</Typography>
+            <Typography>
+              {' '}
+              Access your flashcards from any device, at any time. Study on the go with ease.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Box sx={{my: 6, textAlign: 'center'}}>
+        <Typography variant="h4" component="h2" gutterBottom>Pricing</Typography>
+        <Grid container spacing={4} justifyContent="center">
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Basic</Typography>
+            <Typography variant="h6">$5 / month</Typography>
+            <Typography>
+              {' '}
+              Limited Storage
+            </Typography>
+            <Button variant ="contained" color="primary">
+              Get
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Pro</Typography>
+            <Typography variant="h6">$10 / month</Typography>
+            <Typography>
+              {' '}
+              Limited Storage
+            </Typography>
+            <Button variant ="contained" color="primary">
+              Get
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Basic</Typography>
+            <Typography variant="h6">$5 / month</Typography>
+            <Typography>
+              {' '}
+              Unlimited Storage
+            </Typography>
+            <Button variant ="contained" color="primary">
+              Get
+            </Button>
+          </Grid>
+          
+        </Grid>
+      </Box>
+
+
+    </Container>
+  )
+}
+/*
+
+
+
+
+*/
+
+
+
